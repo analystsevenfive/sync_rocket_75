6ea6d348726dd7c932895c72e8271a31b8e1c400
Sonthep <sonthep@users.noreply.github.com>
2026-09-05 15:37:13 +0700

    test: check Col1 for BKIN tickets

--- github_action/poc-login.js
+++ github_action/poc-login.js
@@ -4,40 +4,22 @@ async function main() {
   const auth = await rocket.rocketLogin();
   console.log('LOGIN OK');
 
-  // Fetch ticket_view.php?id=7634722708
-  const res = await fetch('https://rocket75.com/main/ticket_view.php?id=7634722708', {
-    headers: { Cookie: auth.cookie }
-  });
-  const html = await res.text();
-  console.log('ticket_view length:', html.length);
-
-  // Search for BK_SERVICE
-  console.log('Contains BK_SERVICE?:', html.includes('BK_SERVICE'));
-  if (html.includes('BK_SERVICE')) {
-    const idx = html.indexOf('BK_SERVICE');
-    console.log('Context of BK_SERVICE:', html.substring(Math.max(0, idx - 200), idx + 200));
-  }
-
-  // Dump all form groups or labels
-  const regex = /<label[^>]*>([\s\S]*?)<\/label>[\s\S]*?<div[^>]*>([\s\S]*?)<\/div>/gi;
-  let m;
-  while ((m = regex.exec(html)) !== null) {
-    const l = rocket.cleanText(m[1]);
-    const v = rocket.cleanText(m[2]);
-    if (l && v && l.length < 50 && v.length < 150) {
-      console.log('LABEL:', l, '=>', v);
+  const tickets = ['BKIN0826-000581', 'BKIN0826-000608', 'BKIN0826-000757', 'BKIN0826-000736'];
+  for (const t of tickets) {
+    const html = await rocket.getParentTicketHtml(auth, '01/08/2026', '12/09/2026', '1', t);
+    const rowRegex = /<tr[^>]*>([\s\S]*?)<\/tr>/gi;
+    let m;
+    while ((m = rowRegex.exec(html)) !== null) {
+      if (m[1].includes(t)) {
+        const cellRegex = /<td[^>]*>([\s\S]*?)<\/td>/gi;
+        let cMatch;
+        const cells = [];
+        while ((cMatch = cellRegex.exec(m[1])) !== null) {
+          cells.push(rocket.cleanText(cMatch[1]));
+        }
+        console.log(`[${t}] Col0: ${cells[0]} | Col1: ${cells[1]} | Col3: ${cells[3]}`);
+      }
     }
-  }
-
-  // Also check sub-ticket ticket_checkrepair_view.php?id=6077859363
-  const subRes = await fetch('https://rocket75.com/main/ticket_checkrepair_view.php?id=6077859363', {
-    headers: { Cookie: auth.cookie }
-  });
-  const subHtml = await subRes.text();
-  console.log('Sub contains BK_SERVICE?:', subHtml.includes('BK_SERVICE'));
-  if (subHtml.includes('BK_SERVICE')) {
-    const idx = subHtml.indexOf('BK_SERVICE');
-    console.log('Sub context of BK_SERVICE:', subHtml.substring(Math.max(0, idx - 200), idx + 200));
   }
 }
 
