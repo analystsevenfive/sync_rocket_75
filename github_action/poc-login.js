451ab08fa9055d2ef9276eb85027adbb6293f843
Sonthep <sonthep@users.noreply.github.com>
2026-09-05 15:46:07 +0700

    test: search for รัตนา in BKIN0826-000736

--- github_action/poc-login.js
+++ github_action/poc-login.js
@@ -2,20 +2,38 @@ const rocket = require('./lib/rocket-client');
 
 async function main() {
   const auth = await rocket.rocketLogin();
-  const res = await fetch('https://rocket75.com/hr/organization_chart.php', {
+  const html = await rocket.getParentTicketHtml(auth, '01/08/2026', '12/09/2026', '1', 'BKIN0826-000736');
+  const pMatch = html.match(/ticket_view\.php\?id=(\d+)/i);
+  if (!pMatch) {
+    console.log('Not found BKIN0826-000736');
+    return;
+  }
+  const parentId = pMatch[1];
+  console.log('BKIN0826-000736 Parent ID:', parentId);
+
+  // Check checkrepair
+  const crHtml = await rocket.getCheckRepairHtml(parentId, auth);
+  console.log('crHtml has รัตนา?:', crHtml.includes('รัตนา'));
+  const subIds = rocket.extractCheckRepairIds(crHtml);
+  console.log('Sub IDs:', subIds);
+  for (const sId of subIds) {
+    const sHtml = await rocket.getTicketDetailHtml(sId, auth);
+    console.log(`Sub ${sId} has รัตนา?:`, sHtml.includes('รัตนา'));
+    if (sHtml.includes('รัตนา')) {
+      const idx = sHtml.indexOf('รัตนา');
+      console.log(`Sub context:`, sHtml.substring(Math.max(0, idx - 150), idx + 150));
+    }
+  }
+
+  // Also check parent ticket_view
+  const pRes = await fetch('https://rocket75.com/main/ticket_view.php?id=' + parentId, {
     headers: { Cookie: auth.cookie }
   });
-  const html = await res.text();
-  console.log('Org chart length:', html.length);
-  console.log('Has แววดาว?:', html.includes('แววดาว'));
-  if (html.includes('แววดาว')) {
-    const idx = html.indexOf('แววดาว');
-    console.log('Context แววดาว:', html.substring(Math.max(0, idx - 100), idx + 100));
-  }
-  console.log('Has รัตนา?:', html.includes('รัตนา'));
-  if (html.includes('รัตนา')) {
-    const idx = html.indexOf('รัตนา');
-    console.log('Context รัตนา:', html.substring(Math.max(0, idx - 100), idx + 100));
+  const pHtml = await pRes.text();
+  console.log('pHtml has รัตนา?:', pHtml.includes('รัตนา'));
+  if (pHtml.includes('รัตนา')) {
+    const idx = pHtml.indexOf('รัตนา');
+    console.log(`Parent context:`, pHtml.substring(Math.max(0, idx - 150), idx + 150));
   }
 }
 
